2e07b72979fc64908585ce19aea3a5bc6f9b06fc
OliHornby <olihornby@manchesterdigital.com>
2026-05-12 14:30:30 +0100

    changed root url for vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: './',
+  root: '/mdtechactive/dist/',
   base: '/mdtechactive/dist/',
   plugins: [react()],
   server: {
