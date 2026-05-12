f352f3af9345bc218202a3e58f7911cdd65aea75
OliHornby <olihornby@manchesterdigital.com>
2026-05-12 10:47:16 +0100

    chaned base link for vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   root: './',
+  base: '/mdtechactive/',
   plugins: [react()],
   server: {
     host: '127.0.0.1',
